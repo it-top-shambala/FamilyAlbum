c9a7046413b006b3b52c0a912b2ff35d57df3e2d
Starinin Andrey <starininandrey@gmail.com>
2022-11-15 18:28:32 +0300

    MVVM WPF

---
+++ FamilyAlbum.App/Models/Photo.cs
@@ -0,0 +1,35 @@
+﻿namespace FamilyAlbum.App.Models;
+
+public class Photo : BaseNotification
+{
+    private string _path;
+    public string ImagePath
+    {
+        get => _path; 
+        set => SetField(ref _path, value);
+    }
+
+    private string _comment;
+    public string Comment
+    {
+        get => _comment; 
+        set => SetField(ref _comment, value);
+    }
+
+    public Photo()
+    {
+        
+    }
+
+    public Photo(Photo photo)
+    {
+        Comment = photo.Comment;
+        ImagePath = photo.ImagePath;
+    }
+
+    public Photo(string path, string comment)
+    {
+        ImagePath = path;
+        Comment = comment;
+    }
+}
---
+++ FamilyAlbum.App/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using FamilyAlbum.App.Commands;
+using FamilyAlbum.App.Models;
+using Microsoft.Win32;
+
+namespace FamilyAlbum.App.ViewModels;
+
+public class MainWindowViewModel : BaseNotification
+{
+    private Photo _selectedPhoto;
+    public Photo SelectedPhoto
+    {
+        get => _selectedPhoto; 
+        set => SetField(ref _selectedPhoto, value);
+    }
+
+    private string _comment;
+    public string Comment
+    {
+        get => _comment; 
+        set => SetField(ref _comment, value);
+    }
+    
+    private string _path;
+    public string ImagePath
+    {
+        get => _path; 
+        set => SetField(ref _path, value);
+    }
+
+    public ObservableCollection<Photo> Photos { get; set; }
+
+    public ICommand CommandOpenImage { get; set; }
+    public ICommand CommandSave { get; set; }
+    public ICommand CommandClear { get; set; }
+    public ICommand CommandSelectionItem { get; set; }
+
+    public MainWindowViewModel()
+    {
+        Photos = new ObservableCollection<Photo>();
+        SelectedPhoto = new Photo();
+
+        CommandOpenImage = new LambdaCommand(_ => true, Open);
+        CommandSave = new LambdaCommand(_ => true, Save);
+        CommandClear = new LambdaCommand(_ => true, _ =>
+        {
+            Comment = "";
+            ImagePath = "";
+        });
+        CommandSelectionItem = new LambdaCommand(_ => true, _ =>
+        {
+            Comment = SelectedPhoto.Comment;
+            ImagePath = SelectedPhoto.ImagePath;
+        });
+    }
+
+    private void Open(object? o)
+    {
+        var openFileDialog = new OpenFileDialog
+        {
+            Filter = "Image files (*.png;*.jpeg)|*.png;*.jpeg|All files (*.*)|*.*"
+        };
+        var result = openFileDialog.ShowDialog();
+        if ((bool)result)
+        {
+            ImagePath = openFileDialog.FileName;
+        }
+    }
+
+    private void Save(object? o)
+    {
+        Photos.Add(new Photo(ImagePath, Comment));
+        //TODO Копирование файла
+        //TODO ДОбавление в БД
+    }
+}
