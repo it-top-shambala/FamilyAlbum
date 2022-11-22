826ce5141d53620af9f21a6707a4ea7f4ff71b18
Starinin Andrey <starininandrey@gmail.com>
2022-11-22 18:01:19 +0300

    Add DataBase proj

---
+++ FamilyAlbum.DataBase/Photo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FamilyAlbum.DataBase;
+
+public class Photo
+{
+    public int Id { get; set; }
+    public string Path { get; set; }
+    public string Comment { get; set; }
+
+    public Photo(string path, string comment)
+    {
+        Path = path;
+        Comment = comment;
+    }
+
+    public void SetFields(string path, string comment)
+    {
+        Path = path;
+        Comment = comment;
+    }
+}
---
+++ FamilyAlbum.DataBase/PhotosDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FamilyAlbum.DataBase;
+
+public sealed class PhotosDbContext : DbContext
+{
+    public DbSet<Photo> Photos { get; set; }
+
+    public PhotosDbContext()
+    {
+        Database.EnsureCreated();
+        Photos.Load();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=photos.db;");
+    }
+
+    public void AddPhoto(string path, string comment)
+    {
+        Photos.Add(new Photo(path, comment));
+        SaveChanges();
+    }
+
+    public void UpdatePhoto(int id, string path, string comment)
+    {
+        Photos.FirstOrDefault(p => p.Id == id).SetFields(path, comment);
+        SaveChanges();
+    }
+}
